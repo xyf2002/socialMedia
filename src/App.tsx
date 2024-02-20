1cc0bb4dbd77ab14c56295c9f8b09b60d84a9a59
Alex Xia <2316639449@qq.com>
2024-02-20 14:47:05 +0000

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Routes, Route} from 'react-router-dom';
+import './global.css';
+import SigninForm from "./_auth/forms/SigninForm.tsx";
+import SignupForm from "./_auth/forms/SignupForm.tsx";
+import {Home} from "./_root/pages";
+import AuthLayout from "./_auth/AuthLayout.tsx";
+import RootLayout from "./_root/RootLayout.tsx";
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+
+const App = () => {
+    return (
+        <main className="flex h-screen">
+            <Routes>
+                {/* public routes */}
+                <Route element={<AuthLayout />}>
+                    <Route path="/sign-in" element={<SigninForm />} />
+                    <Route path="/sign-up" element={<SignupForm />} />
+                </Route>
+
+                {/* private routes */}
+                <Route element={<RootLayout />}>
+    {/*                <Route index element={<Home />} />
+                    <Route path="/explore" element={<Explore />} />
+                    <Route path="/saved" element={<Saved />} />
+                    <Route path="/all-users" element={<AllUsers />} />
+                    <Route path="/create-post" element={<CreatePost />} />
+                    <Route path="/update-post/:id" element={<EditPost />} />
+                    <Route path="/posts/:id" element={<PostDetails />} />
+                    <Route path="/profile/:id/*" element={<Profile />} />
+                    <Route path="/update-profile/:id" element={<UpdateProfile />} />*/}
+                </Route>
+            </Routes>
+        </main>
+
+    )
 }
+
 
 export default App
---
+++ src/_auth/forms/SigninForm.tsx
@@ -0,0 +1,10 @@
+ import React from "react";
+
+const SigninForm = () => {
+    return (
+        <div>
+        <h1>Signin Form</h1>
+        </div>
+    );
+}
+export default SigninForm;
